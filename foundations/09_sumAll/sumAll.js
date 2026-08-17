8a3f79ed4886e2a7a03a983f3df537a31776dc67
Noah Reeverts <noah.reeverts@gmail.com>
2026-08-17 16:19:42 -0700

    Complete exercises

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (
+    a < 0 ||
+    b < 0 ||
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    Math.floor(a) !== a ||
+    Math.floor(b) !== b
+  ) {
+    return "ERROR";
+  }
+  if (a <= b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
