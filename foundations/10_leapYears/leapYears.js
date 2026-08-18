1a2a231f0ffcd5c147bd8740830c37bc9604a0ed
Noah Reeverts <noah.reeverts@gmail.com>
2026-08-17 18:09:02 -0700

    Complete exercises

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0 && year) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
 
+// function leapYears(year) {
+//   const isYearDivisibleByFour = year % 4 === 0;
+//   const isCentury = year % 100 === 0;
+//   const isYearDivisibleByFourHundred = year % 400 === 0;
+//
+//   if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 // Do not edit below this line
 module.exports = leapYears;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let temp = ((5 / 9) * (f - 32)).toFixed(1);
+  return +temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let temp = ((9 / 5) * c + 32).toFixed(1);
+  return +temp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
