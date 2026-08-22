b2edb5284b1b9808eabadcc63067cc841c4e6fd5
Noah Reeverts <noah.reeverts@gmail.com>
2026-08-22 12:58:03 -0700

    Complete execise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+  let arr = str.split(" ");
+  str = arr.join("");
 
+  let i = 0;
+  let j = str.length - 1;
+ 
+  while (i < j) {
+    if (str[i] != str[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
 };
 
 // Do not edit below this line
